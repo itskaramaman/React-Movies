b93b9f4c5d1a4b25358262dbbf7b2d96ce011781
Karamjeet Singh <janduk@student.douglascollege.ca>
2023-03-06 13:30:32 -0800

    worked on mobile movie details ui

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -22,26 +22,26 @@ const MovieDetails = () => {
   }, [id]);
 
   return (
-    <div className="flex justify-between bg-gray-800 px-10 pb-6">
-      <div className="basis-1/3">
+    <div className="md:flex md:justify-between bg-gray-800 md:px-10 md:pb-6">
+      <div className="md:basis-1/3">
         {details.poster_path && (
           <img
             src={`https://image.tmdb.org/t/p/original/${details.poster_path}`}
             alt="celeb poster"
-            className="details-img"
+            className="details-img mx-auto pt-5"
           />
         )}
       </div>
-      <div className="basis-2/3 text-white">
-        <h1 className="text-3xl mb-5">{details.title}</h1>
+      <div className="p-5 md:basis-2/3 text-white">
+        <h1 className="text-xl md:text-3xl md:mb-5">{details.title}</h1>
         <p className="text-gray-300">{details.overview}</p>
         <section className="flex justify-between">
           {/* genres */}
           <div className="my-3">
-            <h1 className="text-xl">Genres</h1>
+            <h1 className=" text-sm md:text-xl">Genres</h1>
             <ul className="ml-5">
               {details.genres?.map((genre) => (
-                <li className="list-disc" key={genre.id}>
+                <li className="text-xs md:text-base list-disc" key={genre.id}>
                   {genre.name}
                 </li>
               ))}
@@ -49,16 +49,18 @@ const MovieDetails = () => {
           </div>
 
           <div className="my-3">
-            <h1 className="text-xl">Released Date</h1>
-            <li className="list-disc">{details.release_date}</li>
+            <h1 className="text-sm md:text-xl">Released Date</h1>
+            <li className="text-xs md:text-base list-disc">
+              {details.release_date}
+            </li>
           </div>
 
           {/* production companies */}
           <div className="my-3">
-            <h1 className="text-xl">Production Companies</h1>
+            <h1 className="text-sm md:text-xl">Production Companies</h1>
             <ul className="ml-5">
               {details.production_companies?.map((company) => (
-                <li className="list-disc" key={company.id}>
+                <li className="text-xs md:text-base list-disc" key={company.id}>
                   {company.name}
                 </li>
               ))}
@@ -67,8 +69,10 @@ const MovieDetails = () => {
         </section>
 
         <section className="mt-5">
-          <h1 className="text-xl mb-3">View Similar Movies</h1>
-          <div className="flex justify-between flex-wrap">
+          <h1 className="text-center md:text-start text-xl mb-3">
+            View Similar Movies
+          </h1>
+          <div className="flex flex-col items-center md:justify-between md:flex-wrap md:flex-row">
             {similarMovies.map((movie) => {
               return (
                 movie.poster_path !== null && (
